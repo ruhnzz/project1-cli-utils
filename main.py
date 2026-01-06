7b1f995a49390e9f33747a0970e81abf4acee2fc
ruhnzz <imruheena18@gmail.com>
2026-01-06 17:07:27 +0530

    files added

---
+++ main.py
@@ -0,0 +1,2 @@
+print("Welcome to CLI UTILITY SUITE:")
+print("here we offer you:\n1.Check if a number is prime\n2.Print all primes in a range\n3.Count digits of a number\n4.Check character type (upper/lower/digit)\n5.Multiply N numbers")
